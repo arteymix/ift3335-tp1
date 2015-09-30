68572ed295fc81c51068ebc65ddb4e5b0e1a2da8
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2015-09-30 19:40:50 -0400

    Corrige la condition de validation d'une grille
    Utilise 'bincount' au lieu de 'stats.mode' afin de trouver la fréquence
    de l'élément le plus fréquent.

--- sudoku.py
+++ sudoku.py
@@ -11,17 +11,22 @@ class Sudoku(Problem):
     L'état initial est supposé valide.
     """
     def actions(self, state):
-        for i, j in np.where(state == 0):
+        print np.where(state == 0)
+        for i, j in zip(*np.where(state == 0)):
             for k in range(1, 10):
                 new_state = np.array(state)
-                new_state.itemset([i, j], k)
+                new_state.itemset((i, j), k)
 
                 line = new_state[i]
                 column = new_state[:,j]
                 square = new_state[i//3:i//3+3,j//3:j//3+3]
 
-                # valide la nouvelle configuration
-                if all(lambda x: x <= 1, map(stats.mode, [line[line>0], column[column>0], square[square>0]])):
+                # valide la nouvelle configuration et s'assurant qu'une même
+                # valeur non-nulle n'apparait pas plus d'une fois dans la ligne,
+                # colonne et carré correspondant
+                if all([x.max() <= 1 for x in map(np.bincount, [line[line.nonzero()],
+                                                                column[column.nonzero()],
+                                                                square[square.nonzero()].flatten()])]):
                     yield new_state
 
     def goal_test(self, state):
